ea390605b8563f41606dfece03bece5f42d9a407
navanjane <nkindelpitiya@gmail.com>
2021-07-22 13:33:48 +0530

    db config added

--- lms/settings/production.py
+++ lms/settings/production.py
@@ -1,1 +1,16 @@
 from .base import *
+
+from .base import *
+
+ALLOWED_HOSTS = ['139.59.238.153']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lanka',
+        'USER': 'lanka',
+        'PASSWORD': config('db_password'),
+        'HOST': 'private-lankainstitutedb-do-user-9562704-0.b.db.ondigitalocean.com',
+        'PORT': '25060',
+    }
+}
