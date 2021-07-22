545a5a2c353ffc1dd4ad60c597f302857f57ee41
navanjane <nkindelpitiya@gmail.com>
2021-07-22 13:58:16 +0530

    Object Storage config added

--- lms/settings/production.py
+++ lms/settings/production.py
@@ -18,3 +18,18 @@ DATABASES = {
 INSTALLED_APPS += [
     'storages'
 ]
+
+
+
+AWS_ACCESS_KEY_ID = 'CQCKAKNPLGAAQEPHVTNR'
+AWS_SECRET_ACCESS_KEY = 'EQR+a5wXEVgNS2CLT2RorGEdis5zrZp45TJ7+maruYs'
+AWS_STORAGE_BUCKET_NAME = 'lankaspace'
+AWS_S3_ENDPOINT_URL = 'https://sgp1.digitaloceanspaces.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'DATA'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
